b9a983b9592a4580b6c91fb6495ae8b6068375a5
MarcoCarega <52409054+MarcoCarega@users.noreply.github.com>
2020-08-28 16:37:07 +0200

    statistiche e movimento(base) online e funzionanti (vi aspettavate un messaggio ignorante eh?)

--- New Unity Project/Assets/Scripts/UNET/PlayerController.cs
+++ New Unity Project/Assets/Scripts/UNET/PlayerController.cs
@@ -5,16 +5,26 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
-    [SyncVar] public int cannon;
+    private CustomManager netManager;
+
+    [SyncVar] public int cannon; //Componenti
     [SyncVar] public int armor;
     [SyncVar] public int engine;
     [SyncVar] public int wheel;
 
-    private CustomManager netManager;
+    public float speed; //Statistiche
+    public float acceleration;
+    public float attack;
+    public float defense;
+    public float maneuverability;
+
+    private Vector3 velocity; //Variabili per movimento
+    private float drag;
 
     // Start is called before the first frame update
     void Start()
     {
+        initMovementThings();
         netManager = GameObject.Find("NetworkManager").GetComponent<CustomManager>();
         if (isLocalPlayer)
         {
@@ -22,7 +32,7 @@ public class PlayerController : NetworkBehaviour
             NetworkVehicle net = GetComponent<NetworkVehicle>();
             CmdSetComponents(net.cannon, net.armor, net.engine, net.wheel);
         }
-        else if(!hasAuthority)
+        else if (!hasAuthority)
         {
             createVehicle();
         }
@@ -31,25 +41,30 @@ public class PlayerController : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isLocalPlayer)
+        {
+            MoveCommands();
+        }
     }
 
+    //Comandi e RPC
+
     [Command]
-    public void CmdSetComponents(int cannon,int armor,int engine,int wheel)
+    public void CmdSetComponents(int cannon, int armor, int engine, int wheel)
     {
         this.cannon = cannon;
         this.armor = armor;
         this.engine = engine;
         this.wheel = wheel;
         print("CreateVehicle() Command...");
-        if(!isClient) createVehicle();
+        if (!isClient) createVehicle();
         RpcSetComponents(cannon, armor, engine, wheel);
     }
 
     [ClientRpc]
     public void RpcSetComponents(int cannon, int armor, int engine, int wheel)
     {
-        print("CANNONS: "+this.cannon + "----" + cannon);
+        print("CANNONS: " + this.cannon + "----" + cannon);
         this.cannon = cannon;
         print("CANNON DOPO:" + this.cannon);
         this.armor = armor;
@@ -57,6 +72,85 @@ public class PlayerController : NetworkBehaviour
         this.wheel = wheel;
         print("createVehicle RPC...");
         createVehicle();
+    }
+
+    [Command]
+    public void CmdRotate(float angle)
+    {
+        if (!isClient)
+        {
+            transform.Rotate(0, angle, 0);
+        }
+        RpcRotate(angle);
+    }
+
+    [ClientRpc]
+    public void RpcRotate(float angle)
+    {
+         transform.Rotate(0, angle, 0);
+    }
+
+    [Command]
+    public void CmdTranslate(Vector3 velocity)
+    {
+        if(!isClient)
+        {
+            transform.Translate(velocity);
+        }
+        RpcTranslate(velocity);
+    }
+
+    [ClientRpc]
+    public void RpcTranslate(Vector3 velocity)
+    {
+        transform.Translate(velocity);
+    }
+
+    //Funzioni supporto
+
+    private void initMovementThings()
+    {
+        velocity = Vector3.zero;
+        drag = 0.02f;
+    }
+
+    private void MoveCommands()
+    {
+        float dump = 1 - drag;
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.z -= GetVelocity();
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            velocity.z += GetVelocity();
+        }
+        else velocity.z *= dump;
+        if (Input.GetKey(KeyCode.A))
+        {
+            CmdRotate(-GetAngle());
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            CmdRotate(GetAngle());
+        }
+        velocity.z = ResizeVelocity(velocity.z);
+        CmdTranslate(velocity);
+    }
+
+    private float GetVelocity()
+    {
+        return acceleration * Time.deltaTime;
+    }
+
+    private float GetAngle()
+    {
+        return maneuverability;
+    }
+
+    private float ResizeVelocity(float velocity)
+    {
+        return (velocity > 0) ? Mathf.Min(velocity, speed) : Mathf.Max(velocity, -speed);
     }
 
     private void createVehicle()
@@ -69,7 +163,7 @@ public class PlayerController : NetworkBehaviour
         set.Add("cannon", prefab[cannon].GetComponent<VehicleComponent>());
         set.Add("armor", prefab[armor].GetComponent<VehicleComponent>());
         set.Add("engine", prefab[engine].GetComponent<VehicleComponent>());
-        //setStats(set);
+        setStats(set);
         GameObject vehicle = build(set);
         vehicle.transform.SetParent(transform);
         print("CreateVehicle Succeeded!");
@@ -92,6 +186,15 @@ public class PlayerController : NetworkBehaviour
         VehicleComponent cannon = createCannon(board, set["cannon"]);
         cannon.transform.SetParent(board.transform);
         return board;
+    }
+
+    private void setStats(Dictionary<string,VehicleComponent> set)
+    {
+        speed = set["engine"].values[0];
+        acceleration = set["engine"].values[1];
+        attack = set["cannon"].values[0];
+        defense = set["armor"].values[0];
+        maneuverability = set["armor"].values[1] + set["wheel"].values[0];
     }
 
     private VehicleComponent createCannon(GameObject board, VehicleComponent vehicleComponent)
